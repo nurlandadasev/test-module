434a775ef5381273f79e0a25d15f3abaa38d6ff8
Nurlan <nurlandadasev@gmail.com>
2022-02-18 17:01:10 +0400

    initial commin

---
+++ src/main/java/com/example/testmodule/TestModuleApplication.java
@@ -0,0 +1,16 @@
+package com.example.testmodule;
+
+import com.example.testmodulesecond.entities.Users;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestModuleApplication.class, args);
+    }
+
+
+}

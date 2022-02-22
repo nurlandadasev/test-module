f63fd82f47b89b476bb109be77c5abde71636e77
Nurlan <nurlandadasev@gmail.com>
2022-02-22 11:34:37 +0400

    Update TestModuleApplication.java

--- src/main/java/com/example/testmodule/TestModuleApplication.java
+++ src/main/java/com/example/testmodule/TestModuleApplication.java
@@ -18,6 +18,6 @@ public class TestModuleApplication implements CommandLineRunner {
         Users users = new Users();
 
         System.out.println(users);
-        System.out.println("NEw COMMITSSSSS!!!!!!");
+        System.out.println("NEw COMMITSSSSSSSAAAS!!!!!!");
     }
 }

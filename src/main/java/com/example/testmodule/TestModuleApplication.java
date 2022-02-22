0521bc3c098cbe6610b22b073c790383757676e4
Nurlan <nurlandadasev@gmail.com>
2022-02-22 11:29:02 +0400

    Update TestModuleApplication.java

--- src/main/java/com/example/testmodule/TestModuleApplication.java
+++ src/main/java/com/example/testmodule/TestModuleApplication.java
@@ -6,15 +6,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TestModuleApplication {
+public class TestModuleApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
-        Users users = new Users();
-
-        System.out.println(users);
-        System.out.println("HIIII MAAAAN!!!!!!");
         SpringApplication.run(TestModuleApplication.class, args);
     }
 
 
+    @Override
+    public void run(String... args) throws Exception {
+        Users users = new Users();
+
+        System.out.println(users);
+        System.out.println("HIIII MAAAAN!!!!!!");
+    }
 }
